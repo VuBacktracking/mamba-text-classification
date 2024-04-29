0325a116ad31cc7c32fc4c679e6093d50383dba8
VuBacktracking <vubacktracking@users.noreply.github.com>
2024-04-29 15:17:23 +0700

    Update mamba head

--- mamba/head.py
+++ mamba/head.py
@@ -5,8 +5,8 @@ class MambaClassificationHead(nn.Module):
     def __init__(self, d_model, num_classes, **kwargs):
         super(MambaClassificationHead, self).__init__()
         
-        # Sử dụng một lớp tuyến tính để thực hiện phân loại dựa trên đầu vào có kích thước d_model và num_classes cần phân loại.
+        # Use a linear layer to perform classification based on the input with size d_model and the number of classes to classify num_classes.
         self.classification_head = nn.Linear(d_model, num_classes, **kwargs)
+        
     def forward(self, hidden_states):
         return self.classification_head(hidden_states)
-    
