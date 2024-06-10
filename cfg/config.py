7a41e58c2d3e84528676855c0b5a926afb40151a
VuBacktracking <vubacktracking@users.noreply.github.com>
2024-06-10 12:40:46 +0700

    Update config and fix the error

--- cfg/config.py
+++ cfg/config.py
@@ -2,18 +2,21 @@ import json
 from dataclasses import field, asdict
 
 class MambaConfig:
-    d_model: int  = 2560
+    d_model: int = 2560
+    d_intermediate: int = 0
     n_layer: int = 64
-    vocab_size: int  = 50277
-    ssm_cfg: dict = field(default_factory = dict)
+    vocab_size: int = 50277
+    ssm_cfg: dict = field(default_factory=dict)
+    attn_layer_idx: list = field(default_factory=list)
+    attn_cfg: dict = field(default_factory=dict)
     rms_norm: bool = True
     residual_in_fp32: bool = True
     fused_add_norm: bool = True
-    padd_vocab_size_multiple: int = 8
-    tie_embeddings = None
-    
+    pad_vocab_size_multiple:int = 8
+    tie_embeddings: bool = True
+
     def to_json_string(self):
         return json.dumps(asdict(self))
-    
+
     def to_dict(self):
         return asdict(self)
